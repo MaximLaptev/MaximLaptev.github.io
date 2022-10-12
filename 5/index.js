b7573e9f517eb3863b51d3ab9ec75745908c144c
MaximLaptev <112850591+MaximLaptev@users.noreply.github.com>
2022-10-12 18:10:35 +0300

    Update index.js

--- 5/index.js
+++ 5/index.js
@@ -5,24 +5,24 @@ function calculator() {
     let price2 = price[0].value;
     let count2 = count[0].value;
     let result = document.getElementById("result");
-    let f1 = count2.match(/^\d+$/);
-    let f2 = price2.match(/^\d+$/);
-    if (f1 !== null && f2 !== null)
+    let answer1 = count2.match(/^\d+$/);
+    let answer2 = price2.match(/^\d+$/);
+    if (answer1 !== null && answer2 !== null)
     {
         result.innerHTML = parseInt(price[0].value) * parseInt(count[0].value);
     }
 
-    else if (f1 === null && f2 === null) 
+    else if (answer1 === null && answer2 === null) 
     {
         result.innerHTML = "Данных нет или введены некорректно!";
     }
 
-    else if (f1 === null && f2 !== null) 
+    else if (answer1 === null && answer2 !== null) 
     {
         result.innerHTML = "Количество не введено или введено некорректно!";
     }
 
-    else if (f2 === null && f1 !== null )
+    else if (answer2 === null && answer1 !== null )
     {
         result.innerHTML = "Цена не введена или введена некорректно!";
     }
